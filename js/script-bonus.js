6ed9add975ece514343bf0d89569e24c52897eda
Vito Gentile <gentilevito1076@gmail.com>
2020-12-11 05:44:13 +0100

    BONUS: Un po' di Formattazione

---
+++ js/script-bonus.js
@@ -0,0 +1,13 @@
+var nome = prompt('Qual è il tuo nome?');
+console.log('Il suo nome è: ' + nome);
+
+var cognome = prompt('Ciao, ' + nome + ' qual è il tuo cognome?');
+console.log('Il suo cognome è: ' + cognome);
+
+var colorePreferito = prompt('Qual è il tuo colore preferito?');
+console.log('Il suo colore preferito è: ' + colorePreferito);
+
+var annoCorrente = new Date().getFullYear();
+
+var password = document.getElementById('output');
+password.innerText = 'Password: ' + nome + cognome + colorePreferito + annoCorrente;
