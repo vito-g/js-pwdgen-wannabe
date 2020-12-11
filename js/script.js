02effdbd71ca3dfbf0662ff12962e23931e8d5c1
Vito Gentile <gentilevito1076@gmail.com>
2020-12-11 05:01:40 +0100

    Terzo Prompt + Generatore anno corrente + Password generata

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@ console.log('Il suo nome è: ' + nome);
 
 var cognome = prompt('Qual è il tuo cognome?');
 console.log('Il suo cognome è: ' + cognome);
+
+var colorePreferito = prompt('Qual è il tuo colore preferito?');
+console.log('Il suo colore preferito è: ' + colorePreferito);
+
+var annoCorrente = new Date().getFullYear();
+
+var password = document.getElementById('output');
+password.innerText = nome + cognome + colorePreferito + annoCorrente;
